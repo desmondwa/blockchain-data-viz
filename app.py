e18d1d7ab6ba1eeeba518a2ca9cad88e17916ffa
desmondwa <desmond.wareham@gmail.com>
2022-02-20 14:04:34 -0500

    dataframe rendering in browser as json

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 import psql 
+import pandas
 # from env import API_KEY
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def browser():
+#     df = psql.load_data('public','daily_gas_used')
+#     return "hello"
+
 @app.route('/')
 def browser():
-    return psql.load_data(public.daily_gas_used)
+   data = psql.load_data('public','daily_gas_used')
+   json_data = data.to_json(orient='values')
+   return json_data
+  
+
+# connect to other datasources and compare w/
+# plotly with flask for python viz or create csv + tableau 

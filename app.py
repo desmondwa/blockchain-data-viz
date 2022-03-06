3ef0f162d7a981f7c0878e4b9c510c9f92774ea1
desmondwa <desmond.wareham@gmail.com>
2022-03-06 11:59:56 -0500

    plotly line graphs rendering

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import psql 
 import pandas
+import plotly.express as px
+
 # from env import API_KEY
 
 app = Flask(__name__)
@@ -15,7 +17,13 @@ def browser():
    data = psql.load_data('public','daily_gas_used')
    json_data = data.to_json(orient='values')
    return json_data
+
+@app.route('/line')
+def line():
+   df = psql.load_data('public','daily_gas_used')
+   fig = px.scatter(df, x="collected_for_day", y="gas_used")
+   fig.show()
   
 
 # connect to other datasources and compare w/
-# plotly with flask for python viz or create csv + tableau 
+# plotly with flask + python viz or create csv + tableau 
